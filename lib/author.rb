8de2de44551b8c9064df35f3470c782b1b8d18b5
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-02 17:07:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,8 @@
 class Author
     attr_accessor :author, :name
 
-    def initialize(author, name)
-        @author = author
-        @name = name
-    end
 
-    author_name = Author.new("author", "name")
+    Hillary = Author.new
 
 
-end 
+end
