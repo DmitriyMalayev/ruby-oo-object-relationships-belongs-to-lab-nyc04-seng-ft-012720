69e345bbd0aa28a7274752939b9392b233080f3b
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-02 16:59:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist
     attr_accessor :name, :artist
-    def initialize(name, artist)
+
+    def initialize
     @name = name
     @artist = artist
     end
 
     artist_name = Artist.new("song", "artist")
 
-end 
+end
